a2ac750d5af3b1e2e572e25a707e288848fbb67e
xmugenx <xmugenx@users.noreply.github.com>
2019-10-17 20:17:00 -0400

    bug fix

--- extend.php
+++ extend.php
@@ -1,20 +1,21 @@
 <?php
+
 use Flarum\Extend;
 use Flarum\Foundation\Event\Validating;
 use Flarum\Discussion\DiscussionValidator;
 use Illuminate\Events\Dispatcher;
 
 return function (Dispatcher $events) {
-        $events->listen(Validating::class, function (Validating $event){
-                if ($event->type instanceof DiscussionValidator) {
-                        $rules = $event->validator->getRules();
-                        foreach ($rules['title'] as $k => $v) {
-                        if (strpos($v, 'min:') === 0) {
-                                $rules['title'][$k] = 'min:1';
-                        }
+    $events->listen(Validating::class, function (Validating $event) {
+        if ($event->type instanceof DiscussionValidator) {
+            $rules = $event->validator->getRules();
+            foreach ($rules['title'] as $k => $v) {
+                if (strpos($v, 'min:') === 0) {
+                    $rules['title'][$k] = 'min:1';
                 }
-                $event->validator->setRules($rules);
             }
-        });
-    };
+            $event->validator->setRules($rules);
+        }
+    });
+};
 
