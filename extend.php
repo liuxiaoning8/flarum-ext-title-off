75c75bd29afb283975eecf02ad8ef4927992c5e0
xmugenx <xmugenx@users.noreply.github.com>
2019-10-17 20:07:51 -0400

    fix some bug

--- extend.php
+++ extend.php
@@ -6,14 +6,14 @@ use Illuminate\Events\Dispatcher;
 
 return function (Dispatcher $events) {
         $events->listen(Validating::class, function (Validating $event){
-            if ($event->type instanceof DiscussionValidator) {
-                $event->validator->setRules([
-                    'title' => [
-                        'required',
-                        'min:1',
-                        'max:80'
-                    ]
-                ]);
+                if ($event->type instanceof DiscussionValidator) {
+                        $rules = $event->validator->getRules();
+                        foreach ($rules['title'] as $k => $v) {
+                        if (strpos($v, 'min:') === 0) {
+                                $rules['title'][$k] = 'min:1';
+                        }
+                }
+                $event->validator->setRules($rules);
             }
         });
     };
